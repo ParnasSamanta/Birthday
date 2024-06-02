f56d30f8038ed25a70d7acaa04333118b3fe88c1
Parnas Samanta <parnassamanta07@gmail.com>
2024-06-02 20:41:53 +0530

    first commit

---
+++ countdown.js
@@ -0,0 +1,37 @@
+let e = document.querySelector(".countdown");
+let p = document.querySelector("page0");
+let counttime = 1;//11
+const tick = () => {
+    const k = setInterval(() => {
+        e.innerHTML = `<div>${counttime-1}</div>`;
+        counttime--;
+        if (counttime <= 0) {
+            e.innerHTML = "<div>Let's Go!</div>";
+        }
+        if (counttime < 0) {
+            clearInterval(k);
+            window.scrollBy({
+                behavior:"smooth",
+                top: window.innerHeight
+            });
+            Run();
+            setTimeout(function() {
+                stopAnimation = true;
+                c.style.visibility = "hidden";
+                confetti.start();
+                anim();
+              }, 10000);
+            setTimeout(function() {
+                window.scrollBy({
+                    behavior:"smooth",
+                    top: window.innerHeight
+                });
+                Canvas.style.visibility = "hidden";
+                confetti.stop();  
+            },21047)
+            enableScroll();
+        }
+
+    },1000);
+};
+window.addEventListener('click',tick);
---
+++ script.js
@@ -0,0 +1,74 @@
+
+const stopConfetti = () => {
+    setTimeout (function() {
+        confetti.stop();
+    }, 8000);//interval for which confetti would fall
+};
+
+stopConfetti();
+
+// const autoscroll = () => {
+//         window.scrollBy({
+//             behavior:"smooth",
+//             top: window.innerHeight
+//         });
+// }
+// function scrollCallback () {
+//     for(let i=1; i<=7; i++){
+//         setTimeout (function() {
+//             autoscroll();
+//         })
+//     }
+// }
+// setInterval(autoscroll, 10000);//each slide showing time
+
+// window.addEventListener("scroll", function(){
+//     if(window.scrollY >= window.innerHeight){
+//         //confetti.start(); //do something
+//         stopConfetti();
+//     }
+// })
+
+//Gsap Animation
+
+gsap.from("#page2 .images",{
+    scale:0,
+    opacity:0,
+    duration:2,
+    scrollTrigger:{
+        trigger:"#page2 .images",
+        scroller:"body",
+        start:"top 60%"
+    }
+})
+
+//vedio playback
+
+const vids = document.querySelectorAll("video");
+window.onscroll = function (e) {
+    e = window.scrollY;
+    for (let i = 2; i <= vids.length; i++) {
+        if (e == i*window.innerHeight) {
+            vids[i-2].play();
+            vids[i-2].onended = function () {
+                window.scrollBy({
+                    behavior:"smooth",
+                    top: window.innerHeight
+                });
+            }
+        } else {
+            vids[i-2].pause();
+            vids[i-2].currentTime = 0;
+        }
+    }
+    
+};
+
+// dataUrl = c.toDataURL();
+// your_div.style.background='url('+dataUrl+')';
+
+//preventscroll
+
+// window.onload = function() {
+//     disableScroll();
+// }
